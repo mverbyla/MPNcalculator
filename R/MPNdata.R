ce198230374e8c5e6fd9252b2367e4104ba41c54
mverbyla <33550011+mverbyla@users.noreply.github.com>
2019-12-17 00:52:10 -0800

    Add files via upload

---
+++ R/MPNdata.R
@@ -0,0 +1,22 @@
+#' Example set of Quanti-Tray 2000 data for multiple samples
+#'
+#' Data from multiple IDEXX Colilert and Enterolert analyses on water
+#' samples, with one or more dilution levels for each sample. For use
+#' with demonstrations and examples for the MPNcalcalator package.
+#'
+#' @docType data
+#'
+#' @usage data(MPNdata)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://safewater.sdsu.edu}{The Safe WaTER Lab at SDSU}
+#'
+#' @examples
+#' data(MPNdata)
+"MPNdata"
